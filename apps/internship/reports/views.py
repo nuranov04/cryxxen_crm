279f70df334b1d33b087a3fefe8eb46fc2fbd01b
Saidulloh <ssavutokhunov@gmail.com>
2023-01-03 00:46:28 +0600

    create view ReportApiViewSet

--- apps/internship/reports/views.py
+++ apps/internship/reports/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
 
-# Create your views here.
+from apps.internship.reports.models import Report
+from apps.internship.reports.serializers import ReportSerializer
+
+
+class ReportApiViewSet(GenericViewSet,
+                       mixins.CreateModelMixin):
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
+
+    def perform_create(self, serializer):
+        return serializer.save(intern=self.request.user)
