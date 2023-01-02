f1469dd572539138e3322b2a9a0d433acb9982bf
Saidulloh <ssavutokhunov@gmail.com>
2023-01-03 00:45:01 +0600

    add method get_user_group

--- apps/internship/groups/views.py
+++ apps/internship/groups/views.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
+from rest_framework.decorators import action
 
 from utils.permissions import IsIntern
 from apps.internship.groups.models import Bunch
@@ -21,3 +22,9 @@ class BunchApiViewSet(GenericViewSet,
         if self.action == "retrieve":
             return BunchRetrieveSerializer
         return BunchSerializer
+
+    @action(
+        detail=False, methods=["get"]
+    )
+    def get_user_groups(self, request):
+        return Bunch.objects.filter(members_in=request.user)
